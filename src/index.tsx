2e68cfefac1904d2753765eba9e126cf81927411
Denis Bokov <denisbokov1703@gmail.com>
2023-04-28 20:06:43 +0200

    `phones catalog`

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom';
+import { HashRouter as ReactRouter } from 'react-router-dom';
+import App from './App';
+
+ReactDOM.render(
+  <ReactRouter>
+    <App />
+
+  </ReactRouter>,
+  document.getElementById('root'),
+);
